42ecb867ea7c16db930be65635f6d62ee045eb03
Felipe Chiesa <felipechiesa05@gmail.com>
2022-09-16 11:08:53 -0300

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+<div className="navbar bg-rose-800">
+    <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">WC22</a>
+    </div>
+    <div className="flex-none">
+        <ul className="menu menu-horizontal p-0">
+        <li><a>HOME</a></li>
+        <li tabIndex={0}>
+            <a>
+            SHOP
+            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+            </a>
+            <ul className="p-2 bg-base-100">
+            <li><a>HOME KITS</a></li>
+            <li><a>AWAY KITS</a></li>
+            </ul>
+        </li>
+        <li><a>CONTACT US</a></li>
+        </ul>
+    </div>
+    </div>
+    )
+}
+
+export default NavBar
